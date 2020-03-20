a7e09137b1a188ffe0698d358d2c9b2c0afb26a5
gage117 <57643501+gage117@users.noreply.github.com>
2020-03-20 17:26:58 -0500

    Added name and description of app to package.json and removed unnecessary console.log

--- src/ingredients/ingredients-router.js
+++ src/ingredients/ingredients-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ingredientsService = require('./ingredients-service');
+const xss = require('xss')
 
 const ingredientsRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -9,12 +10,16 @@ ingredientsRouter
   .get((req, res, next) => {
     ingredientsService.getAllIngredients(req.app.get('db'))
       .then(ingredients => {
+        for (const ingredient of ingredients) {
+          ingredient.name = xss(ingredient.name);
+        }
         res.json(ingredients);
       })
       .catch(next);
   })
   .post(jsonBodyParser, (req, res, next) => {
     const newIngredient = {...req.body};
+
 
     for (const [key, value] of Object.entries(newIngredient))
       if (value == null)
